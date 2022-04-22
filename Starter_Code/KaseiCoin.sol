cfbe00de8e639a682b0244ee67fabcbd010521e6
Maria S <marias.prism@outlook.com>
2022-04-21 22:23:32 -0400

    Add code to KaseiCoin

--- Starter_Code/KaseiCoin.sol
+++ Starter_Code/KaseiCoin.sol
@@ -1,3 +1,6 @@
+/*
+KaseiCoin Mintable
+*/
 pragma solidity ^0.5.0;
 
 //  Import the following contracts from the OpenZeppelin library:
@@ -9,3 +12,15 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
 
 // Create a constructor for the KaseiCoin contract and have the contract inherit the libraries that you imported from OpenZeppelin.
+contract KaseiCoin is ERC20, ERC20Detailed, ERC20Mintable {
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint initial_supply
+    )
+        ERC20Detailed(name, symbol, 18)
+        public
+    {
+        mint(msg.sender, initial_supply);
+    }
+}
